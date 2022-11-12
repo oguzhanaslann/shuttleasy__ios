e84fa98cb77776f6c52f3885f64ae54cb250f34b
oguzhanaslann <oguz21199@gmail.com>
2022-11-12 20:16:50 +0300

    colors and fonts

---
+++ Shuttleasy/ui/theme/FontUtil.swift
@@ -0,0 +1,32 @@
+//
+//  FontUtil.swift
+//  Shuttleasy
+//
+//  Created by Oğuzhan Aslan on 12.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static func poppinsBlack(size: CGFloat) ->  UIFont? {
+        return UIFont(name: "Poppins-Black", size: size)
+    }
+    
+    
+    static func poppinsBold(size: CGFloat) ->  UIFont? {
+        return UIFont(name: "Poppins-Bold", size: size)
+    }
+    
+    static func poppinsSemiBold(size: CGFloat) ->  UIFont? {
+        return UIFont(name: "Poppins-SemiBold", size: size)
+    }
+    
+    static func poppinsMedium(size: CGFloat) ->  UIFont? {
+        return UIFont(name: "Poppins-Medium", size: size)
+    }
+    
+    static func poppinsRegular(size: CGFloat) ->  UIFont? {
+        return UIFont(name: "Poppins-Regular", size: size)
+    }
+}
