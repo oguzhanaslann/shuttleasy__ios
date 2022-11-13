e5a1d8328ba28b9df8041a9a58e1c393fa787667
oguzhanaslann <oguz21199@gmail.com>
2022-11-13 10:14:05 +0300

    - launch screen

---
+++ Pods/Target Support Files/Pods-Shuttleasy-ShuttleasyUITests/Pods-Shuttleasy-ShuttleasyUITests-dummy 2.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shuttleasy_ShuttleasyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Shuttleasy_ShuttleasyUITests
+@end
---
+++ Pods/Target Support Files/Pods-Shuttleasy-ShuttleasyUITests/Pods-Shuttleasy-ShuttleasyUITests-umbrella 2.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Shuttleasy_ShuttleasyUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Shuttleasy_ShuttleasyUITestsVersionString[];
+
