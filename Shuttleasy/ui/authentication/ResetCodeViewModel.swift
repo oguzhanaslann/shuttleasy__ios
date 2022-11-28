03685daa9c49bd7e339b6e51932df8ef28adcf59
Oğuzhan Aslan <58479425+oguzhanaslann@users.noreply.github.com>
2022-11-28 12:32:33 +0300

    reset code vm

--- Shuttleasy/ui/authentication/ResetCodeViewModel.swift
+++ Shuttleasy/ui/authentication/ResetCodeViewModel.swift
@@ -1,1 +1,31 @@
 import Foundation
+import Combine
+
+class ResetCodeViewModel : ViewModel {
+    
+    let authenticator : Authenticator
+   
+    let resetCodeResult = PassthroughSubject<Bool, Error>()
+  
+    let resetCodeSendTask : Task<Bool,Error>? = nil 
+
+    init( authenticatior : Authenticator) {
+        self.authenticator = authenticatior
+    }
+  
+    func sendResetCodeTo(email: String) {
+      
+       if (resetCodeSendTask?.isCancelled == false) {
+          resetCodeSendTask?.cancel()
+       }
+       
+       resetCodeSendTask = Task.init{
+            do {
+              let result = try await self.authenticator(email: email)
+              self.resetCodeResult.send(result)
+            } catch {
+               self.resetCodeResult.send(completion: .failure(error))
+            }
+        }
+    }
+}
